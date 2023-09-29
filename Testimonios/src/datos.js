7fd1ae0d26e92e29c3fe9d1b2e509a419ea99fce
SandyAstorga <sandyastorga13@gmail.com>
2023-09-28 19:46:41 -0600

    testimonios

--- Testimonios/src/datos.js
+++ Testimonios/src/datos.js
@@ -2,7 +2,7 @@ const testimonios = [
     {
     nombre:'Shawn Wang',
     pais:'Singapur',
-    imagen:'/assets/imagen-Shawn.png',
+    imagen:'/images/imagen-Shawn.png',
     cargo:'Ingeniero de Software',
     empresa:'Amazon',
     testimonio:'Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. FreeCodeCamp cambió mi vida'
@@ -10,7 +10,7 @@ const testimonios = [
     {
     nombre:'Sarah Chima',
     pais:'Nigeria',
-    imagen:'/assets/imagen-Sarah.png',
+    imagen:'/images/imagen-Sarah.png',
     cargo:'Ingeniera de Software',
     empresa:'ChatDesk',
     testimonio:'FreeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
@@ -18,7 +18,7 @@ const testimonios = [
     {
     nombre:'Emma Bostian',
     pais:'Suecia',
-    imagen:'/assets/imagen-Emma.png',
+    imagen:'/images/imagen-Emma.png',
     cargo:'Ingeniera de Software',
     empresa:'Spotify',
     testimonio:'Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
